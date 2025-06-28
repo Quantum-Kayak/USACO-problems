3cd454e0cce8d0cf8198eaf6d1e34d80951e6259
Reynold <97868585+Quantum-Kayak@users.noreply.github.com>
2025-06-28 11:02:58 -0700

    Create appledivision.cpp

---
+++ bronze/unstarred/easy/appledivision/appledivision.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <cassert>
+#include <fstream>
+#include <bitset>
+#include <numeric>
+
+using namespace std;
+
+// Type Aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using vpi = vector<pii>;
+using vpl = vector<pll>;
+
+// Constants
+const ll INF = 1e18;
+const int MX = 2e5 + 5;
+
+// Fast IO for USACO
+void setIO(const string& name = "") {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+// Debugging Macros
+#define dbg(x) cerr << #x << " = " << (x) << '\n'
+#define dbg2(x, y) cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << '\n'
+
+// Debug for vectors
+template <typename A, typename B>
+ostream& operator<<(ostream& os, const pair<A, B>& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << "[";
+    for (int i = 0; i < (int)v.size(); ++i)
+        os << v[i] << (i + 1 == (int)v.size() ? "" : ", ");
+    return os << "]";
+}
+
+#define dbg(x) cerr << #x << " = " << (x) << '\n'
+
+
+// Binary Search Helpers
+template <typename T>
+int lb(const vector<T>& v, T val) {
+    return lower_bound(v.begin(), v.end(), val) - v.begin();
+}
+template <typename T>
+int ub(const vector<T>& v, T val) {
+    return upper_bound(v.begin(), v.end(), val) - v.begin();
+}
+
+// Shortcuts
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define f first
+#define s second
+#define sz(x) (int)(x).size()
+#define each(a, x) for (auto& a : x)
+
+int main() {
+    int n;
+    ll tot = 0;
+    cin >> n;
+    vll weight(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> weight[i];
+        tot += weight[i];
+    }
+    
+    ll min_diff = INF;
+
+    for (int mask = 0; mask < (1 << n); ++mask) {
+        ll groupA = 0, groupB = 0;
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) groupA += weight[i];
+            else groupB += weight[i];
+        }
+        min_diff = min(min_diff, abs(groupA - groupB));
+    }
+    cout << min_diff << "\n";
+}
