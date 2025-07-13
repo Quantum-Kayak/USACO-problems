c5ee214a81ce70ebecd1b296bfd742834bd1a2aa
Reynold <97868585+Quantum-Kayak@users.noreply.github.com>
2025-07-12 17:45:58 -0700

    Create tier I.S.cpp

---
+++ extras/tier I.S.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(x) begin(x), end(x)
+#define f first
+#define s second
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+void setIO(string name = "") {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
+
+void solve() {
+    // your logic goes here
+}
+
+int main() {
+    setIO("problemname"); // change this per problem
+    solve();
+    return 0;
+}
