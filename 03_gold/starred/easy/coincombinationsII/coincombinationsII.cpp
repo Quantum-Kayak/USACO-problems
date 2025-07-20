7aa566d93f5ef0e8758c30c3d9cfb793b15eb244
Reynold <97868585+Quantum-Kayak@users.noreply.github.com>
2025-07-20 11:49:32 -0700

    Update and rename coincombinationsI.cpp to coincombinationsII.cpp

---
+++ 03_gold/starred/easy/coincombinationsII/coincombinationsII.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    for (int& c : coins) cin >> c;
+
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1;
+
+    for (int c : coins)
+        for (int i = c; i <= x; ++i)
+            dp[i] = (dp[i] + dp[i - c]) % MOD;
+
+    cout << dp[x] << "\n";
+    return 0;
+}
